ac18f66ceb8cbb64edb21b80115185ac55e9eb1a
karansxa <karansxa@gmail.com>
2025-02-20 23:26:48 +0530

    npm issue fixed

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,21 @@
 // jest.config.js
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  testMatch: ["**/*.test.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
-};
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'json'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ]
+}
